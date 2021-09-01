d6c6bf9bdc363a1625edaac52e40a45ca7706d6c
Samantha Laine <samanthalaine8@gmail.com>
2021-08-31 21:20:43 -0400

    commit tree 5421b36d6b99724fb5b25875f968b8397c71f6bf

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
-class Person
+require 'pry'
 
+class Person
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
 end
+
+#binding.pry
